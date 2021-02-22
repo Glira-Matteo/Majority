d6ea12061ad9856b58f8aeef36647d9ab32e3be0
Major <79458121+Glira-Matteo@users.noreply.github.com>
2021-02-22 14:35:29 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//functions
+    function get(elem){
+    return document.getElementById(elem);
+}
+
+    function print(text){
+        return console.log(text)
+    }
+    
+    function create(elem){
+        return document.write(elem)
+    }
