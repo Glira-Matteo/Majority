2d5869b0b90e7a14eff583c18a3cf4df65afc306
Major <79458121+Glira-Matteo@users.noreply.github.com>
2021-03-03 11:15:12 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-//functions
+/*
+      Author: Major,
+      Date: Februar-March 2021,
+      Info: This is a libary for helping you with Javascript, it's a simplest
+      version of JavaScript, can be used by beginners or experts, just a perfect
+      library! Hope you like it and enjoy coding :D
+*/
       
     function get(elem){
     return document.querySelector(elem);
