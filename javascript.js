8b092d5d22e9395efa787b673616568ec55d61a7
Major <79458121+Glira-Matteo@users.noreply.github.com>
2021-03-03 11:17:42 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,14 +7,13 @@
 */
       
     function get(elem){
-    return document.querySelector(elem);
-}
-    function print(text){
-        return console.log(text);
+      return document.querySelector(elem);
     }
-    
+    function print(text){
+      return console.log(text);
+    }
     function create(elem){
-        return document.write(elem)
+      return document.write(elem)
     }
     
     
